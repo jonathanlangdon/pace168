f0977baadd8c81ffb694f2d087943273ecda4b41
Jonathan Langdon <langdon.jonathan@gmail.com>
2024-09-07 19:06:10 -0400

    testing webhooks again

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,3 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//   fetch('header.html')
-//     .then(response => response.text())
-//     .then(data => {
-//       document.getElementById('header-placeholder').innerHTML = data;
-//       // Add event listener after content is loaded
-//       const menuTrigger = document.querySelector('.menu-trigger');
-//       const nav = document.querySelector('.main-nav .nav');
-
-//       if (menuTrigger && nav) {
-//         menuTrigger.addEventListener('click', function () {
-//           nav.classList.toggle('active');
-//         });
-//       }
-//     });
-// });
-
 function appendFooter() {
   const currentYear = new Date().getFullYear();
 
--- update-site.php
+++ update-site.php
@@ -1,29 +1,15 @@
 <?php
     $secret = 'Heckyeahwewantssecrets385';
-    
-    // Retrieve the GitHub signature header
-    $githubSignature = $_SERVER['HTTP_X_HUB_SIGNATURE_256'] ?? $_SERVER['HTTP_X_HUB_SIGNATURE'] ?? null;
-    
-    // Log headers for debugging (optional, remove later)
-    file_put_contents('php://stderr', print_r(getallheaders(), true));
-
-    // Ensure the signature is not null
-    if ($githubSignature === null) {
-        http_response_code(400);
-        die('Signature header missing');
-    }
-
-    // Generate our hash for comparison
     $signature = 'sha256=' . hash_hmac('sha256', file_get_contents('php://input'), $secret);
 
     // Verify the request signature
-    if (!hash_equals($signature, $githubSignature)) {
+    if (!hash_equals($signature, $_SERVER['HTTP_X_HUB_SIGNATURE_256'])) {
         http_response_code(403);
         die('Invalid signature');
     }
 
     // Define the path to your git repository
-    $repoPath = '/home/newhulings/public_html/pace168.com';
+    $repoPath = '/home/newhulings/public_html/pace168';
 
     // Change directory to the repository
     chdir($repoPath);
